3a116584f0a44a91a747607112f414c461c267e1
pracka <107337667+pracka@users.noreply.github.com>
2023-10-25 13:00:43 +0000

    conversion of dt

---
+++ 01_basics/03_converOps.js
@@ -0,0 +1,26 @@
+let score = 33
+console.log(typeof score);
+
+let score1 = "43"
+console.log(typeof score1);
+
+let valinNumber = Number(score1)
+console.log(typeof valinNumber);
+
+//"33" => 33
+//"33abs" =>NaN (not a number)
+// true => 1
+
+let isLoggedin = 1
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+//"p" => true
+
+let someNum = 33
+
+let stringNum = String(someNum)
+console.log(stringNum)
+console.log(typeof stringNum)
